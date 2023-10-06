b48b8ca0a685fa699f56da278bae17679371f2e3
Beasted179 <clarios179@gmail.com>
2023-10-06 16:07:47 -0700

    changed to pool

--- Client.js
+++ Client.js
@@ -1,13 +1,15 @@
-import pg from 'pg';
-const { Client } = pg;
+import pg, { Pool } from 'pg';
+const { Pool } = pg;
 import { config } from "dotenv";
 config();
 
 const databaseUrl = process.env.DATABASE_URL;  ;
 
 
-export const dbClient = new Client({
+export const dbClient = new Pool({
     connectionString: databaseUrl,
+    max: 20, // Maximum number of clients in the pool
+    idleTimeoutMillis: 30000,
   });
 
 
