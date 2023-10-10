c8e239d4c20a0015cdceffdb135835659740af08
Beasted179 <clarios179@gmail.com>
2023-10-09 20:59:03 -0700

    client fixing

--- attendance.js
+++ attendance.js
@@ -19,7 +19,54 @@ const client = new Client({
 });
 
 client.once("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
+ // Define a collector to listen for button interactions
+const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 3_600_000 });
+
+collector.on("collect", async (interaction) => {
+  const username = interaction.member.nickname;
+  console.log(username);
+  let attendanceStatus = interaction.customId;
+  console.log(attendanceStatus);
+  try {
+    console.log('Connected to PostgreSQL database');
+
+    // Define the table name for attendance data
+    const tableName = 'attendance_data';
+
+    // Create the table if it doesn't exist
+    const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS ${tableName} (
+        id serial PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        attendance_status VARCHAR(10) NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW()
+      )
+    `;
+    await db.query(createTableQuery);
+
+    let toUser;
+
+    // Insert attendance data into the database
+    const insertQuery = `
+      INSERT INTO ${tableName} (username, attendance_status) VALUES ($1, $2)
+    `;
+    const values = [username, attendanceStatus];
+    await db.query(insertQuery, values);
+
+    toUser = `Attendance choice ${attendanceStatus} recorded for ${username}`;
+
+    await interaction.reply({
+      content: toUser,
+      ephemeral: true,
+    });
+    await db.release();
+  } catch (error) {
+    console.error('Error handling interaction:', error);
+    // Handle the error and send an error response if needed
+    interaction.reply('An error occurred while processing your request.');
+  }
+});
+
 });
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
@@ -37,129 +84,78 @@ client.on("messageCreate", async (message) => {
 });
 
 client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-  let db;
-  db =  await dbClient.connect();
   if (interaction.commandName === "attendance") {
     const yes = new ButtonBuilder()
       .setCustomId("Yes")
       .setLabel("Yes")
       .setStyle(ButtonStyle.Success);
-
+  
     const no = new ButtonBuilder()
       .setCustomId("No")
       .setLabel("No")
       .setStyle(ButtonStyle.Danger);
+  
     const maybe = new ButtonBuilder()
       .setCustomId("Maybe")
       .setLabel("Maybe")
       .setStyle(ButtonStyle.Primary);
-    
+  
     const row = new ActionRowBuilder().addComponents(yes, no, maybe);
-   
+  
     const response = await interaction.reply({
       content: interaction.options.getString("event") || " ",
       components: [row],
-    })
-
-    // Define a collector to listen for button interactions
-    const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 3_600_000 });
+    });
+  } else if (interaction.commandName === "downloadattendance"  ) {
+    if(!interaction.member.roles.cache.some(role => role.name === "Admin")) {
+      await interaction.reply({ 
+        content: "You do not have permission to use this command.", 
+        ephemeral: true });
+        return;
+    } 
+    try {
+      
+       db = await dbClient.connect();
+      // Query to retrieve attendance data (customize as needed)
+      const query = 'SELECT username, attendance_status FROM attendance_data';
+  
+      const result = await db.query(query);
+  
+     
+      const textContent = result.rows.map(row => `${row.username}: ${row.attendance_status}`).join('\n');
+  
+  
+      const fileName = 'attendance.txt';
+  
     
-    collector.on("collect", async (interaction) => {
-      const username = interaction.member.nickname;
-      console.log(username)
-      let attendanceStatus = interaction.customId
-      console.log(attendanceStatus)
-      try {
-        
-   
-
-        console.log('Connected to PostgreSQL database');
-      
-        // Define the table name for attendance data
-        const tableName = 'attendance_data';
-        
-        // Create the table if it doesn't exist
-        const createTableQuery = `
-          CREATE TABLE IF NOT EXISTS ${tableName} (
-            id serial PRIMARY KEY,
-            username VARCHAR(255) NOT NULL,
-            attendance_status VARCHAR(10) NOT NULL,
-            created_at TIMESTAMP DEFAULT NOW()
-          )
-        `;
-        await db.query(createTableQuery);
-      
-        let toUser;
-          // Insert attendance data into the database
-          const insertQuery = `
-            INSERT INTO ${tableName} (username, attendance_status) VALUES ($1, $2)
-          `;
-          const values = [username, attendanceStatus];
-          await db.query(insertQuery, values);
-
-          toUser = `Attendance choice ${attendanceStatus} recorded for ${username}`;
-
-        await interaction.reply({
-          content: toUser,
+      const attachment = new AttachmentBuilder()
+        .setFile(Buffer.from(textContent, 'utf-8'))
+        .setDescription('Members Attendance Roster')
+        .setName(fileName)
+        .setSpoiler(false);
+  
+  
+      await interaction.reply({
+        content: "Here is the attendance roster:",
+        files: [attachment],
+        ephemeral: true, // Optional: Make the response ephemeral
+      }); 
+      console.log(interaction.options)
+      if (interaction.options.getBoolean("delete")) {
+        await db.query('DELETE FROM attendance_data');
+        await interaction.followUp({
+          content: "Attendance roster deleted",
           ephemeral: true,
         });
-        await db.release()
-      } catch (error) {
-        console.error('Error handling interaction:', error);
-        // Handle the error and send an error response if needed
-        interaction.reply('An error occurred while processing your request.');
-      } 
-      });
-
-} else if (interaction.commandName === "downloadattendance"  ) {
-  if(!interaction.member.roles.cache.some(role => role.name === "Admin")) {
-    await interaction.reply({ 
-      content: "You do not have permission to use this command.", 
-      ephemeral: true });
-      return;
-  } 
-  try {
-    
-     db = await dbClient.connect();
-    // Query to retrieve attendance data (customize as needed)
-    const query = 'SELECT username, attendance_status FROM attendance_data';
-
-    const result = await db.query(query);
-
-   
-    const textContent = result.rows.map(row => `${row.username}: ${row.attendance_status}`).join('\n');
-
-
-    const fileName = 'attendance.txt';
-
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    } 
   
-    const attachment = new AttachmentBuilder()
-      .setFile(Buffer.from(textContent, 'utf-8'))
-      .setDescription('Members Attendance Roster')
-      .setName(fileName)
-      .setSpoiler(false);
+
+} 
 
 
-    await interaction.reply({
-      content: "Here is the attendance roster:",
-      files: [attachment],
-      ephemeral: true, // Optional: Make the response ephemeral
-    }); 
-    console.log(interaction.options)
-    if (interaction.options.getBoolean("delete")) {
-      await db.query('DELETE FROM attendance_data');
-      await interaction.followUp({
-        content: "Attendance roster deleted",
-        ephemeral: true,
-      });
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  } 
-}
-console.log("releasing db")
-db.release()
 });
 
 
